0403900b6e132b132067e36a39e38dce20a6ee76
Guy <125807123+guyfoz77@users.noreply.github.com>
2023-09-23 16:35:51 +0100

    Remove UV

--- src/js/domController.js
+++ src/js/domController.js
@@ -20,14 +20,12 @@ function elementBuilder(element, classList, textContent, dataName) {  //element 
 export function currentWeatherUpdater(forecast) {
   const currentTemp = document.querySelector('.currentTemp');
   const currentWind = document.querySelector('.currentWind');
-  const currentUV = document.querySelector('.currentUV');
   const currentHumidity = document.querySelector('.currentHumidity');
   const currentConditionText = document.querySelector('.currentConditionText');
   const currentConditionImage = document.querySelector('.currentConditionImage');
 
   currentTemp.textContent = `${forecast.current.temp_c}°C`;
   currentWind.textContent = `${forecast.current.wind_kph} km/h`;
-  currentUV.textContent = `${forecast.current.uv}`
   currentHumidity.textContent = `${forecast.current.humidity}%`;
   currentConditionText.textContent = forecast.current.condition.text;
   currentConditionImage.src = forecast.current.condition.icon;
