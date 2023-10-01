2894aa8a7fe591b662294e1d99bb5f0b4508675e
Guy <125807123+guyfoz77@users.noreply.github.com>
2023-10-01 15:24:46 +0100

    Remove hourcontainers that have already elapsed

--- src/js/domController.js
+++ src/js/domController.js
@@ -49,19 +49,23 @@ function dayAverageContentBuilder(day, forecast) {
   // elements[1].src = forecast.forecast.forecastday[day].day.condition.icon; //this does not feel like an ideal solution.
   return elements;
 }
-function hourlyContentBuilder(forecast) {
+function hourlyContentBuilder(day, forecast) {
   let hourContainers = [];
-  for(let i = 0; forecast.hour.length > i; i++) {
+  const date = new Date()
+  const currentHour = date.getHours();
+  let fromHour = 0; //determines which hour to build the hourlys from.
+  if (day == 0) fromHour = currentHour + 1;
+  for(fromHour; forecast.hour.length > fromHour; fromHour++) {
     const hourContainer = elementBuilder('div', 'hourContainer', '', '');
     const hourLabel = elementBuilder('h3', '', '', '');
-      (i < 10) ?
-        hourLabel.textContent = `0${i}h`:
-        hourLabel.textContent = `${i}h`;
-    const hourTemp = elementBuilder('h4', '', `${Math.round(forecast.hour[i].temp_c)}°C`, '');
+      (fromHour < 10) ?
+        hourLabel.textContent = `0${fromHour}h`:
+        hourLabel.textContent = `${fromHour}h`;
+    const hourTemp = elementBuilder('h4', '', `${Math.round(forecast.hour[fromHour].temp_c)}°C`, '');
     // const hourRainChance = elementBuilder('h5', '', `${forecast.hour[i].chance_of_rain}`, '');
-    const hourWindSpeed = elementBuilder('h5', '', `${Math.round(forecast.hour[i].wind_kph)}km/h`, '');
+    const hourWindSpeed = elementBuilder('h5', '', `${Math.round(forecast.hour[fromHour].wind_kph)}km/h`, '');
     const hourIcon = elementBuilder('img', 'hourIcon', '', '');
-      hourIcon.src = forecast.hour[i].condition.icon;
+      hourIcon.src = forecast.hour[fromHour].condition.icon;
     hourContainer.append(hourLabel, hourTemp, hourWindSpeed, hourIcon);
     hourContainers.push(hourContainer);
   }
@@ -90,7 +94,7 @@ function tabSwitcher(day, forecast) {
 
   const hourlyContentContainer = document.querySelector('.futureWeatherHourContent');
   hourlyContentContainer.innerHTML = '';
-  const hourlyContent = hourlyContentBuilder(forecast.forecast.forecastday[day]);
+  const hourlyContent = hourlyContentBuilder(day, forecast.forecast.forecastday[day]);
   hourlyContentContainer.append(...hourlyContent);
 }
 function tabLabeler() {
