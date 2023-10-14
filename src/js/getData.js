6b212ebdaaacb15d7f73f8a2c16f892baabc2bf0
Guy <125807123+guyfoz77@users.noreply.github.com>
2023-10-14 14:27:12 +0100

    Add https to api link

--- src/js/getData.js
+++ src/js/getData.js
@@ -1,7 +1,7 @@
 import { add } from "date-fns"; //no longer needed
 
 export async function getForecast(location) {
-  const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=486e25196a914325a9c154932231009&days=3&q=${location}`);
+  const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=486e25196a914325a9c154932231009&days=3&q=${location}`);
   const forecastWeather = await response.json();
   return forecastWeather;
 }
